1cfd72d3e7d0d0c39ecf8da693a5860f1736b913
Todd Halkowski <thalkowski@virtusa.com>
2015-08-26 11:22:22 -0500

    instagram 9 test app changes

---
+++ cell.swift
@@ -0,0 +1,19 @@
+//
+//  cell.swift
+//  ParseStarterProject
+//
+//  Created by Todd Halkowski
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+    
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var username: UILabel!
+    
+    @IBOutlet var message: UILabel!
+    
+}
